5efd9bdc5c4c40c4908f314351a4fa4b36618aea
kimyoonyoung <widesight0810@gmail.com>
2019-05-16 09:28:29 +0900

    upbit check price func added

--- upbit_check_cur_price_threadv2.py
+++ upbit_check_cur_price_threadv2.py
@@ -8,8 +8,9 @@ import time
 from PyQt5.QtGui import QColor
 from upbitpy import Upbitpy
 
-tickers = ["KRW-BTC","KRW-ETH","KRW-EOS","KRW-TRX","KRW-BCH","KRW-ADA"]
-form_class = uic.loadUiType("view2.ui")[0]
+tickers = ["KRW-BTC","KRW-ETH","KRW-EOS","KRW-TRX","KRW-BCH","KRW-ADA", "KRW-LTC",
+           'KRW-XLM', 'KRW-QTUM', 'KRW-BSV']
+form_class = uic.loadUiType("view3.ui")[0]
 
 
 class Worker(QThread):
@@ -26,32 +27,34 @@ class Worker(QThread):
             # 단기 급등여부 판단
             candle_num = 3
             for ticker in all_info:
-                candle_list = upbit.get_minutes_candles(candle_num, ticker['market'])
+                candle_dict = upbit.get_minutes_candles(1, ticker['market'], count=3)
 
-                #todo access dictionary list
-                last_three = candle_list[-3]
-                last_two = candle_list[-2]
-                last = candle_list[-1]
+                last_two = candle_dict[-3]['candle_acc_trade_volume']
+                last_one = candle_dict[-2]['candle_acc_trade_volume']
+                last = candle_dict[-1]['candle_acc_trade_volume']
 
+
+                if "KRW-BTC" == ticker['market']:
+                    print('----거래량-----')
+                    print('현재: ' + str(last))
+                    print('1분전: ' + str(last_one))
+                    print('2분전: ' + str(last_two))
                 #min_value = 9e+20
                 #for data in range( 1, candle_list-1) :
                 #    if data['candle_acc_trade_volume'] < min_value:
                 #        min_value = data['candle_acc_trade_volume']
 
-
-                candle_avg = sum / candle_num - 1
-
-                if last['candle_acc_trade_volume'] > last_two['candle_acc_trade_volume']*3 :
-                    rising = '급등'
+                volume_rising = '-'
+                if last > last_one*2 or (last > last_one*1.5 and last_two > last_two*1.5):
+                    rising = '급증'
 
                 cur_price = ticker['trade_price']
                 volume = ticker['acc_trade_volume_24h']
                 signed_change_rate = ticker['signed_change_rate']
 
+                #print(ticker['market'])
 
-
-                data[ticker['market']] = ticker['market'] + (cur_price,) + \
-                                    (volume,) + (signed_change_rate,) + (rising,)
+                data[ticker['market']] = (cur_price,) +  (volume,) + (signed_change_rate,) + (volume_rising,)
 
 
             # 작업이 완료됐을때 이벤트 발생(emit)
@@ -88,22 +91,21 @@ class MyWindow(QMainWindow, form_class):
     @pyqtSlot(dict)
     def update_table_widget(self,data):
 
-
-        for ticker, info in data:
+        for ticker, infos in data.items():
             i = tickers.index(ticker)
 
             # 현재 상승장 여부를 읽어옴
             self.tableWidget.setItem(i, 0, QTableWidgetItem(ticker))
             self.tableWidget.setItem(i, 1, QTableWidgetItem(str(infos[0])))
-            self.tableWidget.setItem(i, 2, QTableWidgetItem(str(infos[3])))
-            # 상승장이면 빨간색으로 넣어줌
-            if infos[3] == "상승장" :
-                self.tableWidget.item(i, 2).setBackground(QColor(255, 0, 0))
 
             #self.tableWidget.setItem(i, 3, QTableWidgetItem(str('%.4f' % float(infos[4]))))
             #self.tableWidget.setItem(i, 4, QTableWidgetItem(str(infos[5] + '%')))
-            self.tableWidget.setItem(i, 5, QTableWidgetItem(str('%.4f' % infos[1])))
-            self.tableWidget.setItem(i, 6, QTableWidgetItem(str('%.4f' % infos[2])))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(str('%.4f' % infos[1])))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(str('%.2f' % (infos[2]*100)+'%')))
+
+            # 상승장이면 빨간색으로 넣어줌
+            if infos[3] == "급증":
+                self.tableWidget.item(i, 4).setBackground(QColor(255, 0, 0))
 
 
 app = QApplication(sys.argv)
